201b782d05730495d149be5c253eedbaf9441606
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2023-06-06 12:40:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Doctorstra/Calculator /Eva
+  git clone https://github.com/Doctorstra/Auto /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
